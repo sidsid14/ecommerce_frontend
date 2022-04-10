2715c8d835373c4e450a386f5e2d9c0d8a9d2e7e
Sudhanshu Tiwari <sudhanshu@Sudhanshus-MacBook-Air.local>
2022-04-10 16:52:29 +0530

    Add routing, find products by category id

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -10,9 +10,10 @@ export class ProductService {
   private baseUrl = 'http://localhost:8080/api/products';
   constructor(private httpClient: HttpClient) {}
 
-  getProductList(): Observable<Product[]> {
+  getProductList(categoryId: number): Observable<Product[]> {
+    const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${categoryId}`;
     return this.httpClient
-      .get<GetResponse>(this.baseUrl)
+      .get<GetResponse>(searchUrl)
       .pipe(map((response) => response._embedded.products));
   }
 }

33ae7e0206b01bd35583f81c64e42933118fcc71
Sudhanshu Tiwari <sudhanshu@Sudhanshus-MacBook-Air.local>
2022-04-12 11:33:22 +0530

    Add items to cart

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
+import { CartItem } from 'src/app/common/cart-item';
 import { GetResponseProducts, Product } from 'src/app/common/product';
+import { CartService } from 'src/app/services/cart.service';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -20,7 +22,8 @@ export class ProductListComponent implements OnInit {
 
   constructor(
     private productService: ProductService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private cartService: CartService
   ) {}
 
   ngOnInit(): void {
@@ -90,6 +93,7 @@ export class ProductListComponent implements OnInit {
   }
 
   addToCart(product: Product) {
-    console.log(product.name);
+    const carItem = new CartItem(product);
+    this.cartService.addToCart(carItem);
   }
 }
---
+++ src/app/services/cart.service.ts
@@ -0,0 +1,62 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import { CartItem } from '../common/cart-item';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CartService {
+  cartItems: CartItem[] = [];
+  totalPrice: Subject<number> = new Subject<number>();
+  totalQuantity: Subject<number> = new Subject<number>();
+
+  constructor() {}
+
+  addToCart(cartItem: CartItem) {
+    const existingCartItem: CartItem = this.cartItems.find(
+      (item) => item.id === cartItem.id
+    )!;
+
+    let alreadyExistsInCart: boolean = existingCartItem !== undefined;
+
+    if (alreadyExistsInCart) {
+      existingCartItem.quantity++;
+    } else {
+      this.cartItems.push(cartItem);
+    }
+
+    this.computeCartTotals();
+  }
+
+  computeCartTotals() {
+    let totalPriceValue: number = 0;
+    let totalQuantityValue: number = 0;
+
+    for (let currentCartItem of this.cartItems) {
+      totalPriceValue += currentCartItem.quantity * currentCartItem.unitPrice;
+      totalQuantityValue += currentCartItem.quantity;
+    }
+
+    this.totalPrice.next(totalPriceValue);
+    this.totalQuantity.next(totalQuantityValue);
+
+    this.logCartData(totalPriceValue, totalQuantityValue);
+  }
+
+  logCartData(totalPriceValue: number, totalQuantityValue: number) {
+    for (let currentCartItem of this.cartItems) {
+      const subTotalPrice =
+        currentCartItem.quantity * currentCartItem.unitPrice;
+      console.log(
+        `name: ${currentCartItem.name}, quantity: ${currentCartItem.quantity},
+         unitPrice: ${currentCartItem.unitPrice}, subTotalPrice: ${subTotalPrice}`
+      );
+    }
+    console.log(
+      `totalPrice: ${totalPriceValue.toFixed(
+        2
+      )}, totalQuantity: ${totalQuantityValue}`
+    );
+    console.log('---');
+  }
+}

e96c8b678b3a58a914ece732c13e1f05616ea572
Sudhanshu Tiwari <sudhanshu@Sudhanshus-MacBook-Air.local>
2022-04-15 14:55:40 +0530

    populate credit card dropdowns

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormService } from 'src/app/services/form.service';
 
 @Component({
   selector: 'app-checkout',
@@ -10,8 +11,13 @@ export class CheckoutComponent implements OnInit {
   checkoutFormGroup!: FormGroup;
   totalPrice: number = 0;
   totalQuantity: number = 0;
+  creditCardYears: number[] = [];
+  creditCardMonths: number[] = [];
 
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private formService: FormService
+  ) {}
 
   ngOnInit(): void {
     this.checkoutFormGroup = this.formBuilder.group({
@@ -43,10 +49,37 @@ export class CheckoutComponent implements OnInit {
         expirationYear: [''],
       }),
     });
+
+    const startMonth = new Date().getMonth() + 1;
+    this.getMonthOptions(startMonth);
+
+    this.formService
+      .getCreditCardYears()
+      .subscribe((data) => (this.creditCardYears = data));
   }
 
   onSubmit() {
     console.log('Handling the submit button');
     console.log(this.checkoutFormGroup.get('customer')?.value);
+  }
+
+  handleMonthsAndYears() {
+    const creditCardFormGroup = this.checkoutFormGroup.get('creditCard');
+
+    const currentYear = new Date().getFullYear();
+    const selectedYear = Number(creditCardFormGroup?.value.expirationYear);
+
+    let startMonth: number = 1;
+
+    if (currentYear === selectedYear) {
+      startMonth = new Date().getMonth() + 1;
+    }
+    this.getMonthOptions(startMonth);
+  }
+
+  private getMonthOptions(startMonth: number) {
+    this.formService
+      .getCreditCardMonths(startMonth)
+      .subscribe((data) => (this.creditCardMonths = data));
   }
 }

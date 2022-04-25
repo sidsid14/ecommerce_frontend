92782d71339e06c1115cc9005c16cceb973c7f8e
Sudhanshu Tiwari <sudhanshu@Sudhanshus-MacBook-Air.local>
2022-04-25 11:08:46 +0530

    checkout form complete

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {
+  FormArray,
   FormBuilder,
   FormControl,
   FormGroup,
@@ -10,6 +11,11 @@ import { Country } from 'src/app/common/country';
 import { FormService } from 'src/app/services/form.service';
 import { FormValidators } from 'src/app/validators/form-validators';
 import { CartService } from 'src/app/services/cart.service';
+import { CheckoutService } from 'src/app/services/checkout.service';
+import { Router } from '@angular/router';
+import { Order } from 'src/app/common/order';
+import { OrderItem } from 'src/app/common/order-item';
+import { Purchase } from 'src/app/common/purchase';
 
 @Component({
   selector: 'app-checkout',
@@ -30,7 +36,9 @@ export class CheckoutComponent implements OnInit {
   constructor(
     private formBuilder: FormBuilder,
     private formService: FormService,
-    private cartService: CartService
+    private cartService: CartService,
+    private checkoutService: CheckoutService,
+    private router: Router
   ) {}
 
   ngOnInit(): void {
@@ -210,24 +218,103 @@ export class CheckoutComponent implements OnInit {
     });
   }
 
+  /* 
+   Returns an array of invalid control/group names, or a zero-length array if 
+   no invalid controls/groups where found 
+*/
+  // private findInvalidControlsRecursive(
+  //   formToInvestigate: FormGroup | FormArray
+  // ): string[] {
+  //   var invalidControls: string[] = [];
+  //   let recursiveFunc = (form: FormGroup | FormArray) => {
+  //     Object.keys(form.controls).forEach((field) => {
+  //       const control = form.get(field);
+  //       if (control?.invalid) invalidControls.push(field);
+  //       if (control instanceof FormGroup) {
+  //         recursiveFunc(control);
+  //       } else if (control instanceof FormArray) {
+  //         recursiveFunc(control);
+  //       }
+  //     });
+  //   };
+  //   recursiveFunc(formToInvestigate);
+  //   return invalidControls;
+  // }
   onSubmit() {
-    console.log(this.checkoutFormGroup.value);
     if (this.checkoutFormGroup.invalid) {
       this.checkoutFormGroup.markAllAsTouched();
+      return;
     }
-    console.log(this.checkoutFormGroup.get('customer')?.value);
-    console.log(
-      `shipping address ` +
-        this.checkoutFormGroup.get('shippingAddress')?.value.country.name +
-        `, ` +
-        this.checkoutFormGroup.get('shippingAddress')?.value.state.name
+
+    //set up order
+    let order = new Order(this.totalQuantity, this.totalPrice);
+
+    //get cart items
+    const cartItems = this.cartService.cartItems;
+
+    //create orderItems from cartItems
+    let orderItems: OrderItem[] = cartItems.map((item) => new OrderItem(item));
+
+    //set up purchase
+    let purchase = new Purchase();
+
+    //populate purchase customer
+    purchase.customer = this.checkoutFormGroup.controls['customer'].value;
+
+    //populate purchase - shipping address
+    purchase.shippingAddress =
+      this.checkoutFormGroup.controls['shippingAddress'].value;
+    const shippingState: State = JSON.parse(
+      JSON.stringify(purchase.shippingAddress.state)
     );
-    console.log(
-      `billing address ` +
-        this.checkoutFormGroup.get('billingAddress')?.value.country.name +
-        `, ` +
-        this.checkoutFormGroup.get('billingAddress')?.value.state.name
+    const shippingCountry: Country = JSON.parse(
+      JSON.stringify(purchase.shippingAddress.country)
     );
+    purchase.shippingAddress.state = shippingState.name;
+    purchase.shippingAddress.country = shippingCountry.name;
+
+    //populate purchase - billing address
+    purchase.billingAddress =
+      this.checkoutFormGroup.controls['billingAddress'].value;
+    const billingState: State = JSON.parse(
+      JSON.stringify(purchase.billingAddress.state)
+    );
+    const billingCountry: Country = JSON.parse(
+      JSON.stringify(purchase.billingAddress.country)
+    );
+    purchase.billingAddress.state = billingState.name;
+    purchase.billingAddress.country = billingCountry.name;
+
+    //populate purchase - order and orderitems
+    purchase.order = order;
+    purchase.orderItems = orderItems;
+
+    //call REST API via the checkout service
+    this.checkoutService.placeOrder(purchase).subscribe({
+      next: (response) => {
+        alert(
+          `Your order has been received.\nOrder tracking number: ${response.orderTrackingNumber}`
+        );
+        //reset cart
+        this.resetCart();
+      },
+      error: (err) => {
+        alert(`There was an error: ${err.message}`);
+      },
+    });
+  }
+
+  resetCart() {
+    //reset cart data
+    this.cartService.cartItems = [];
+    this.cartService.totalPrice.next(0);
+    this.cartService.totalQuantity.next(0);
+
+    //reset the form
+    this.checkoutFormGroup.reset();
+
+    //navigate back to the products page
+    this.router.navigateByUrl('/products');
   }
 
   copyShippingAddressToBillingAddress(event: any) {
--- src/app/validators/form-validators.ts
+++ src/app/validators/form-validators.ts
@@ -1,11 +1,13 @@
 import { FormControl, ValidationErrors } from '@angular/forms';
 
 export class FormValidators {
-  static notOnlyWhitespace(control: FormControl): ValidationErrors {
+  static notOnlyWhitespace(
+    control: FormControl
+  ): { [key: string]: any } | null {
     if (control.value !== null && control.value.trim().length === 0) {
       return { notOnlyWhitespace: true };
     } else {
-      return { notOnlyWhitespace: false };
+      return null;
     }
   }
 }
